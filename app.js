c45a6258ddae9840dc0d2777bd88138cdd57c421
Umaralixan <umaralixtreme@gmail.com>
2025-11-30 15:22:21 +0500

    js

---
+++ app.js
@@ -0,0 +1,11 @@
+// 1 masala
+// let Numbers = (arr) => arr.filter((n) => n % 2 === 0);
+// console.log(Numbers([1, 2, 3, 4, 5, 6]));
+
+// 2 masala
+// const fn = (arr) => Math.max(...arr);
+// console.log(fn ([4, 15, 2, 9]));
+
+// 3 masala
+// const fn = (arr) => arr.map((str) => str.length);
+// console.log(fn(["sardor", "dunyo", "umarali"]));
